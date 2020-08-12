c602fad535c75f58cc2a9262482f230eeea7814e
sami-aloosi <kingxsam@gmail.com>
2020-08-12 14:30:35 -0700

    create-react-app done

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello from the App component</div>
+    </div>
+  );
+}
+
+export default App;
